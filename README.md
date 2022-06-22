de83b4b8e3f45e941bcd0fd12a222dcfd97f01f6
ThinkerHua <thinkerhua@hotmail.com>
2022-06-22 18:48:32 +0800

    Ver1.1.0
    增加接口函数free_dallocstr，专供VB.net使用
    同时修复内部函数replace的BUG

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # SectionSteelAW
- 用于生成型钢单位面积或单位重量的动态链接库。详情参见"help.txt"文档。
- 
- 本项目是 https://github.com/ThinkerHua/SteelStructureCalculationTool.git 的内核重制版。
- 原项目完全采用VB.net编写的，内存占用有点高。所以用C做一个动态链接库，期望能解决内存占用高的问题。
- 
- 未来将从原项目剥离出用户界面，用于进行外部数据交互，内核部分引用本DLL执行。
+ 用于生成型钢单位面积或单位重量的计算式的动态链接库。
+ 详情参见"help.txt"文档。
--- dll.h
+++ dll.h
@@ -1,8 +1,6 @@
 #ifndef _DLL_H_
 #define _DLL_H_
 
-#define BUILDING_DLL 1
-
 #if BUILDING_DLL
 #define DLLIMPORT __declspec(dllexport)
 #else
@@ -12,7 +10,7 @@
 DLLIMPORT char *SectionSteelAW(char const *RawText, unsigned const CtrlCode);
 DLLIMPORT void free_dallocstr(char *str);
 /*
-	本函数专供VB使用 
+	本函数专供VB.net使用 
 	C语言可直接使用free释放动态分配的字符串 
 */
 
